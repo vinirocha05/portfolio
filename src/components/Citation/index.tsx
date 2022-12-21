3f9dee9fc0961af6a805869fa9719b9e08e66a51
Vinicius Rocha <vinirocha05@gmail.com>
2022-12-21 07:22:04 -0300

    ajuste texto citação

--- src/components/Citation/index.tsx
+++ src/components/Citation/index.tsx
@@ -6,12 +6,12 @@ export default function Citation() {
       <h2>Apresentação</h2>
       <p>
         <span>&quot; </span>
-        Olá meu nome é Vinicius, atualmente trabalho com negócios digitais em um
-        dos maiores bancos do Brasil. O contato com a área de Tecnologia
+        Olá, meu nome é Vinicius, atualmente trabalho com negócios digitais em
+        um dos maiores bancos do Brasil. O contato com a área de Tecnologia
         despertou em mim a vontade de conhecer um pouco mais da área de
-        Desenvolvimento. Atualmente estou estudando React e Typescript e
-        gostaria de usar esses conhecimentos para desenvolver projetos que
-        impactem na vida de milhares de pessoas
+        Desenvolvimento. Atualmente estudo React e Typescript e gostaria de usar
+        esses conhecimentos para desenvolver projetos que impactem na vida de
+        milhares de pessoas
         <span> &quot;</span>
       </p>
     </Container>
